a9127a026e6aecd0157ae5815e22aef7b726885f
W-Bjwa04 <waleedshahid123ml@gmail.com>
2025-01-31 13:19:20 +0500

    fix images path

--- src/App.jsx
+++ src/App.jsx
@@ -135,7 +135,7 @@ const App = () => {
       <div className='wrapper'>
         <header>
 
-          <img src="../public/hero-img.png" alt="Hero Banner" />
+          <img src="/hero-img.png" alt="Hero Banner" />
           <h1>
             Find <span className='text-gradient'>Movies</span> You'll Enjoy Without the Hassle
           </h1>
